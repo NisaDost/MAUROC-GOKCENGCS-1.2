8d037ca8baeab9c7d28938a61c5cff43a13c3dd6
Nisa Dost <i.n.dost38@gmail.com>
2025-03-03 17:04:25 +0300

    fix on creating a node

---
+++ GCS/GCS-CORE/node/Program.cs
@@ -0,0 +1,18 @@
+﻿using Rcl;
+using Rcl.Logging;
+
+namespace node
+{
+    class Program
+    {
+        public static async void Main(string[] args)
+        {
+            await using var context = new RclContext(args);
+            using var node = context.CreateNode("my_ros2_node");
+            var sub = node.CreateSubscription<Std.String>(String.Empty);
+
+            node.Logger.LogInformation($"Hello ROS 2 {RosEnvironment.Distribution}!");
+        }
+    }
+
+}
