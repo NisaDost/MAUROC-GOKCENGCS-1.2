6764efd02e985df92e4fab2cadd7909b7e2935a9
Nisa Dost <i.n.dost38@gmail.com>
2025-03-11 16:45:41 +0300

    Folder structure

---
+++ GCS/GCS-CORE/gcs_node/GCSNode.cs
@@ -0,0 +1,28 @@
+﻿using Rcl;
+using Rcl.Logging;
+
+namespace GCS_Node
+{
+    class GCSNode
+    {
+        public async Task Run(string[] args)
+        {
+
+            await using var context = new RclContext(args);
+            using var node = context.CreateNode("gcs_node");
+            using var pub = node.CreatePublisher<Std.String>("/chatter");
+
+            node.Logger.LogInformation($"TARGET UAV ID PUBLISHER RUNNING!");
+
+            while (true)
+            {
+                var message = new Std.String { Data = "Hello deneme" };
+                pub.Publish(message);
+
+                await Task.Delay(3000);
+
+            }
+        }
+    }
+
+}
---
+++ GCS/GCS-CORE/gcs_node/Program.cs
@@ -0,0 +1,19 @@
+﻿using Rcl;
+using Rcl.Logging;
+
+namespace GCS_Node
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var Node = new GCSNode();
+
+            await Node.Run(args);
+
+
+        }
+    }
+
+}
